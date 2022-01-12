3d0e48c39769fa222fddc4cb0296837ce011ad01
Nick Poole <Nick.Poole@SparkFun.com>
2022-01-12 00:06:31 -0500

    Update batch_program.bat
    - changed fuse bits to set the brownout detection to 2.7v

--- Programming/batch_program.bat
+++ Programming/batch_program.bat
@@ -6,8 +6,10 @@
 @echo Flashing bootloader...
 rem The fuse bits are *extremely* important to get correct. Be careful.
 rem These were found by using ATtiny core and 'Programming Bootloader' and looking at verbose Arduino IDE output
-rem This enables B.O.D at 1.8v and sets the clock source to external 16Mhz xtal
-@avrdude -C avrdude.conf -pattiny841 -cusbtiny -e -Uefuse:w:0b11111110:m -Uhfuse:w:0b11011110:m -Ulfuse:w:0xEE:m 
+rem This enables B.O.D at 2.7v and sets the clock source to external 16Mhz xtal
+@avrdude -C avrdude.conf -pattiny841 -cusbtiny -e -Uefuse:w:0b11111110:m -Uhfuse:w:0b11011101:m -Ulfuse:w:0xEE:m 
+
+
 
 @timeout 1
 
